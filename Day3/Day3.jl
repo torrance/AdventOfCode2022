2765f626c8f50d6297785d39bd1c4bd412103ecf
Torrance Hodgson <torrance123@gmail.com>
2022-12-05 00:41:21 +0000

    Use super cute \cap symbols for intersection

--- Day3/Day3.jl
+++ Day3/Day3.jl
@@ -1,8 +1,8 @@
 function priority(char::Char)
     if islowercase(char)
-        return Int(char) - 96
+        return Int(char) - Int('a') + 1
     elseif isuppercase(char)
-        return Int(char) - 65 + 27
+        return Int(char) - Int('A') + 27
     end
     @assert false "Invalid character"
 end
@@ -12,7 +12,7 @@ rucksacks = map(collect, eachline(joinpath(@__DIR__, "input.txt")))  # collect e
 # Part 1
 prioritysum = sum(rucksacks) do rucksack
     compartment1, compartment2 = eachcol(reshape(rucksack, :, 2))
-    common = only(intersect(compartment1, compartment2))
+    common = only(compartment1 ∩ compartment2)
     return priority(common)
 end
 println("Part 1 sum: ", prioritysum)
@@ -20,8 +20,8 @@ println("Part 1 sum: ", prioritysum)
 # Part 2
 rucksacks = reshape(rucksacks, 3, :)  # group sequential elves into triplets
 
-prioritysum = sum(eachcol(rucksacks)) do triplet
-    common = only(intersect(triplet...))
+prioritysum = sum(eachcol(rucksacks)) do (ruck1, ruck2, ruck3)
+    common = only(ruck1 ∩ ruck2 ∩ ruck3)
     return priority(common)
 end
 println("Part 2 sum: ", prioritysum)
