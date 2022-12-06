bb43a3df33b22c9ad1662f5fafe1d0e7865f0ca3
Torrance Hodgson <torrance123@gmail.com>
2022-12-06 09:17:21 +0000

    Use my own, non-allocating allunique()

--- Day6/Day6.jl
+++ Day6/Day6.jl
@@ -1,6 +1,19 @@
+ # Julia provides `isunique()` O(log n) but this is optimised for large arrays.
+ # For small arrays, the O(n^2) exhaustive search is faster and non-allocating.
+ function alluniqueN2(xs)
+    for (i, x) in enumerate(xs)
+        for (j, y) in enumerate(xs)
+            if i != j && x == y
+                return false
+            end
+        end
+    end
+    return true
+end
+
 function findunique(data, N)
     for (istart, istop) in zip(1:length(data), N:length(data))
-        if allunique(@view data[istart:istop])
+        if alluniqueN2(@view data[istart:istop])
             return istop
         end
     end
@@ -8,7 +21,7 @@ function findunique(data, N)
     throw(ErrorException("No starting index found"))
 end
 
-const data::Vector{UInt8} = read("input.txt")
+const data::Vector{UInt8} = read(joinpath(@__DIR__, "input.txt"))
 
 # Part 1
 println("Start of packet: ", findunique(data, 4))
